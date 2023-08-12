949f2d3f51d7b9ed7ecda409900539645b98846a
toms74209200 <motomotomato@gmail.com>
2023-08-12 09:21:51 +0900

    :sparkles: step4 add HTML

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mediaStreamConstrains = {
+  video: true,
+};
+
+const localVideo = document.querySelector('video');
+
+let localStream;
+
+// Handles success by adding the MediaStream to the video element.
+const gotLocalMeditaStream = mediaStream => {
+  localStream = mediaStream;
+  localVideo.srcObject = mediaStream;
+};
+
+// Handles error by logging a message to the console with the error message.
+const handleLocalMesiaStreamError = error => {
+  console.log('navigator.getUserMedia error: ', error);
+};
+
+// Initializes media stream.
+navigator.mediaDevices.getUserMedia(mediaStreamConstrains)
+  .then(gotLocalMeditaStream)
+  .catch(handleLocalMesiaStreamError);
